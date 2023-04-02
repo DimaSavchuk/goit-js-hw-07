f20e991e2a4b411cfc2cf5614af8786559770592
Youneri <118117711+Youneri@users.noreply.github.com>
2023-04-02 21:02:47 +0300

    required

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,21 +3,49 @@ import { galleryItems } from './gallery-items.js';
 const galleryEl = document.querySelector('.gallery');
 
 
-const markup = galleryItems.map(({ preview, original, description }) => { `
-<li class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</li>
-` });
+const markup = galleryItems.map(({ preview, original, description }) => {
+    return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>
+    `
+});
 
 galleryEl.insertAdjacentHTML('beforeend', markup.join(''));
+galleryEl.addEventListener('click', onClick);
 
+function onClick(event) {
+    event.preventDefault();
+    if (!event.target.classList.contains('gallery__image')) {
+        return;
+    }
 
+    const originalImg = event.target.dataset.source;
+    const descriptionImg = event.target.alt;
+    const modalImg = basicLightbox.create(
+        `
+        <img
+        class="modal__image"
+        src="${originalImg}"
+        alt="${descriptionImg}"
+        />`,
+    );
 
-console.log(galleryItems);
+    modalImg.show();
+
+    event.currentTarget.addEventListener('keydown', onKeydownModal);
+
+    function onKeydownModal(event) {
+        if (event.key === 'Escape') {
+            modalImg.close();
+        }
+        false;
+    }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryImg = document.querySelector('.gallery');
+
+const markup = galleryItems.map(({ preview, original, description }) => {
+    return `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+       <img 
+        class="gallery__image" 
+        src="${preview}" 
+        alt="${description}" 
+        />
+    </a>
+ </li>
+    `
+});
+
+galleryImg.insertAdjacentHTML('beforeend', markup.join(''));
+galleryImg.addEventListener('click', onClick);
+
+function onClick(event) {
+    event.preventDefault();
+    if (!event.target.classList.contains('gallery__image')) {
+        return;
+    }
+
+    const modalImg = new SimpleLightbox('.gallery a', {
+        captionSelector: 'img',
+        alertErrorMessage: 'Image not found, next image will be loaded',
+        captionsData: 'alt',
+        captionPosition: 'bottom',
+        animationSpeed: 250,
+        captionDelay: 0,
+    })
+}
